e796d3fbc3186e33f036e94f1809a6a672ac015e
Alan Alonso <alan.alonso.gaf15@gmail.com>
2022-12-09 15:35:43 -0300

    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AllowSound from "./components/AllowSound";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <AllowSound />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GlobalVolume/index.jsx
@@ -0,0 +1,28 @@
+import { Howler } from "howler";
+import { useState } from "react";
+  import { GlobalVolumeContainer } from "./style";
+
+const GlobalVolume = () => {
+  const [volume, setVolume] = useState(1);
+
+  Howler.volume(volume);
+
+  return (
+    <GlobalVolumeContainer>
+      <div className="input">
+        <input
+          type="range"
+          step="0.01"
+          min="0"
+          max="1"
+          defaultValue="1"
+          onChange={(e) => {
+            setVolume(e.target.value);
+          }}
+        />
+      </div>
+    </GlobalVolumeContainer>
+  );
+};
+
+export default GlobalVolume;
